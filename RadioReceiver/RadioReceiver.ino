acd83bd522275714001d15c48fa4e82d223acd63
Will <code@willo.info>
2014-07-19 11:51:52 +1000

    Removed testing blocks

--- RadioReceiver/RadioReceiver.ino
+++ RadioReceiver/RadioReceiver.ino
@@ -16,7 +16,7 @@ void setup()
 {
   Serial.begin(9600);	// Debugging only
   Serial.println("setup");
-
+  
   // Initialise the IO and ISR
   vw_set_rx_pin(receive_pin);
   vw_set_ptt_inverted(true); // Required for DR3100
@@ -25,11 +25,10 @@ void setup()
   vw_rx_start();       // Start the receiver PLL running
 }
 
+
+
 void loop()
 {
-  char sz[] = "";
-  char *p = sz;
-  char *str;
   
   uint8_t buf[VW_MAX_MESSAGE_LEN];
   uint8_t buflen = VW_MAX_MESSAGE_LEN;
@@ -38,25 +37,10 @@ void loop()
     int i;
     for (int i=0;i<buflen;i++)
     {
-      if (buf[i] == '#')
-      {
-        //Serial.print(" DINGDINGDING "); 
-      }
-
       Serial.write(buf[i]);
       Serial.print("");
     }
     
-    ///TESTING BLOCK
-    Serial.println("");
-    Serial.println("***");
-    while ((str = strtok_r(p, "#", &p)) != NULL) // delimiter is the semicolon
-    {
-      Serial.println(str);
-    }
-    Serial.println("****");
-    /// END TEST BLOCk
- 
     Serial.println("");
   }
 }
